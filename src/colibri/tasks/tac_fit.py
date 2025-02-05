c0f05d0e775e3782501476a5bc155d899afb502b
Chris Walther Andersen <cvvand@gmail.com>
2025-02-05 21:03:50 +0100

    Addded some comments to code

--- src/colibri/tasks/tac_fit.py
+++ src/colibri/tasks/tac_fit.py
@@ -84,14 +84,29 @@ def _log_prob(param_values: list[float],
               input_data: list[float],
               tissue_data: list[float],
               param_bounds: dict[str, (float, float)]) -> float:
+    # Defines the probability distribution used by emcee (below) to sample
+    # the parameter distribution of a fit model
+
+    # Put parameters into dict object (required by model interface)
     params = {}
     for value, name in zip(param_values, param_names):
         params[name] = value
+
+    # Calculate the model given the current parameters
     ymodel = np.array(model(time_data, input_data, **params))
+
+    # __lnsigma is the natural log of the uncertainty of the data (assumed to
+    # be a constant), which is treated like a parameter of the model
     s2 = np.exp(2.0 * params['__lnsigma'])
+
+    # Prior parameter distribution:
+    # Assumed to be uniform with bounds. i.e., if a parameter is outside the
+    # bounds, the likelihood is 0 (so the log-likelihood is -inf)
     for param in params:
         if not param_bounds[param][0] < params[param] < param_bounds[param][1]:
             return -np.inf
+
+    # Calculate and return the log-proability distribution (non-normalised)
     return -0.5 * np.sum((np.array(tissue_data) - ymodel) ** 2 / s2 +
                          np.log(2.0 * np.pi * s2))
 
@@ -105,32 +120,44 @@ def _fit_emcee(time_data: list[float],
                labels: dict[str, str],
                tcut: int,
                output: Optional[str]) -> None:
+    # Sample the posterior parameter distribution space using Monte Carlo
+    # simulations with the emcee-package
 
-    param_start = []
-    param_names = []
-    param_bounds = {}
+    # Parameters need to be unpacked when passed to emcee
+    param_start = []  # Contains the optimised parameters (from an actual fit)
+    param_names = []  # Contains the name of the parameters in the model
+    param_bounds = {}  # Contains the parameter bounds (priors)
     for param in params:
         param_names.append(param)
         param_start.append(params[param]['value'])
         param_bounds[param] = (params[param]['min'], params[param]['max'])
 
+    # Cut data as required
     time_data_cut = time_data[0:tcut]
     input_data_cut = input_data[0:tcut]
     tissue_data_cut = tissue_data[0:tcut]
 
-    n_walkers = 50
-    n_dim = len(param_start)
-    steps = 5000
+    # Set emcee parameters
+    n_walkers = 50  # Number of walkers used to search the space
+    n_dim = len(param_start)  # Dimensionality of the parameter space
+    steps = 5000  # Number of update steps
 
+    # Start the walkers in a gaussian ball around the optimised parameters
     start_p = np.array(param_start) + 1e-5 * np.random.randn(n_walkers, n_dim)
+
+    # Run as a multithreaded pool
     with Pool(8) as pool:
+        # Start MC
         sampler = emcee.EnsembleSampler(n_walkers, n_dim, _log_prob,
                                         args=(param_names, model, time_data_cut,
                                               input_data_cut, tissue_data_cut,
                                               param_bounds),
                                         pool=pool)
         sampler.run_mcmc(start_p, steps, progress=False)
+    print("... done!")
+    print()
 
+    # Plot the history of each walker
     fig, axes = plt.subplots(n_dim, figsize=(10, 7), sharex=True)
     samples = sampler.get_chain()
     for i in range(n_dim):
@@ -149,6 +176,8 @@ def _fit_emcee(time_data: list[float],
         plt.savefig(samples_png_path)
         plt.clf()
 
+    # Try to calculate autocorrelation times
+    # (might fail if "steps" is too small)
     try:
         tau = sampler.get_autocorr_time()
         print("Autocorrelation times:")
@@ -158,6 +187,7 @@ def _fit_emcee(time_data: list[float],
         print("Autocorrelation could not be estimated")
     print()
 
+    # Make corner plot
     flat_samples = sampler.get_chain(discard=0, thin=1, flat=True)
     corner.corner(flat_samples, labels=param_names, truths=param_start)
     if output is None:
@@ -168,6 +198,7 @@ def _fit_emcee(time_data: list[float],
         plt.savefig(corner_png_path)
         plt.clf()
 
+    # Print parameter quantiles
     print("Parameter quantiles (5%, 16%, 50%, 84%, 95%)")
     for i in range(n_dim):
         mcmc = np.percentile(flat_samples[:, i], [5, 16, 50, 84, 95])
